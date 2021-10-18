ea87e0fd4330da1a8f16d80d71290333da3ca856
Cyrille Nicaise <cyrille.ni1@gmail.com>
2021-10-18 15:32:35 +0200

    UserRequest class and repository creation

---
+++ src/Entity/UserRequest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRequestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRequestRepository::class)
+ */
+class UserRequest
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $usermail;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $usermessage;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getUsermail(): ?string
+    {
+        return $this->usermail;
+    }
+
+    public function setUsermail(string $usermail): self
+    {
+        $this->usermail = $usermail;
+
+        return $this;
+    }
+
+    public function getUsermessage(): ?string
+    {
+        return $this->usermessage;
+    }
+
+    public function setUsermessage(string $usermessage): self
+    {
+        $this->usermessage = $usermessage;
+
+        return $this;
+    }
+
+}
